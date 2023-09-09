5e578fd8316d14e40ff72665b11622798bb81606
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 10:13:30 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Navbar from './components/global/Navbar'
+import Footer from './components/global/Footer'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path='/' Component={Home}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
