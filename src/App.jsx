e89827634a131b57dadd60eca1a0fb3a14934753
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 10:05:59 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      app
+    <Routes>
+      <Route path='/' Component={Home}/>
+    </Routes>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-yellow-300">Home</div>
   )
 }
 
