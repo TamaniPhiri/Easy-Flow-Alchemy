11a18c0b7c507dc17501e2a636a9062527aa4895
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 10:13:34 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Navbar from './components/global/Navbar'
-import Footer from './components/global/Footer'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/global/Navbar";
+import Footer from "./components/global/Footer";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' Component={Home}/>
-    </Routes>
-    <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
