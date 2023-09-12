1c7410376b58260efc9a575054c28f34a1f32f0b
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-12 03:29:02 -0700

    changes

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = () => {
         </div>
         <motion.div animate={open?{opacity:1,x:0}:{opacity:0,x:"100%"}} className='absolute top-[100%] bg-black lg:hidden min-h-screen right-0 w-2/3'>    
           <div className='flex flex-col gap-4 p-4'>
-          <Link>Link 1</Link>
-          <Link>Link 2</Link>
-          <Link>Link 3</Link>
+          <Link>Buy</Link>
+          <Link>Latest</Link>
+          <Link>Donate</Link>
           </div>
         </motion.div>
       </nav>
