c50a1f120d9a56e0667782c2f7e9d20643b34cec
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-13 06:32:54 -0700

    change

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -7,21 +7,21 @@ const Navbar = () => {
   const [open, setOpen]=useState(false)
   return (
     <div className='fixed top-0 w-full z-50'>
-      <nav className='relative w-full flex justify-between bg-[#0a0a0a] text-white py-6 px-4 md:px-8 lg:px-12'>
-        <div>logo</div>
+      <nav className='relative w-full flex justify-between bg-[rgb(120,223,214)] text-white py-6 px-4 md:px-8 lg:px-12'>
+        <div>EASY-FLOW-ALCHEMY</div>
         <div className='lg:flex hidden  gap-8 '>
-          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded-lg shadow-lg md:shadow-xl hover:bg-green-500 hover:text-white transition-colors duration-300'>Buy</Link>
-          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded-lg shadow-lg md:shadow-xl hover:bg-green-500 hover:text-white transition-colors duration-300'>Latest</Link>
-          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded-lg shadow-lg md:shadow-xl hover:bg-green-500 hover:text-white transition-colors duration-300'>Donate</Link>
+          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Buy</Link>
+          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Latest</Link>
+          <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Donate</Link>
         </div>
         <div className='lg:hidden flex '>
           <button onClick={()=>setOpen(!open)}>Menue</button>
         </div>
         <motion.div animate={open?{opacity:1,x:0}:{opacity:0,x:"100%"}} className='absolute top-[100%] bg-black lg:hidden min-h-screen right-0 w-2/3'>    
           <div className='flex flex-col gap-4 p-4'>
-          <Link className=' bg-pink-500 rounded'>Buy</Link>
-          <Link className=' bg-pink-500 rounded'>Latest</Link>
-          <Link className=' bg-pink-500 rounded'>Donate</Link>
+          <Link className=' bg-green-500 rounded'>Buy</Link>
+          <Link className=' bg-green-500 rounded'>Latest</Link>
+          <Link className=' bg-green-500 rounded'>Donate</Link>
           </div>
         </motion.div>
       </nav>
