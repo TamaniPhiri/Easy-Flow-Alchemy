e2566e36375f17456144f6bc93c50ac8e1050e6e
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-14 06:35:45 -0700

    change

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
   return (
     <div className='fixed top-0 w-full z-50'>
       <nav className='relative w-full flex justify-between shadow-2xl bg-[rgb(120,223,214)] text-white py-6 px-4 md:px-8 lg:px-12'>
-        <div><span className=''>EASY-FLOW</span><span className='text-green-500'>-ALCHEMY</span></div>
+        <div><span className='font-bold text-lg lg:text-3xl'>EASY-FLOW</span><span className='font-bold text-lg text-green-500'>-ALCHEMY</span></div>
         <div className='lg:flex hidden  gap-8 '>
           <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Buy</Link>
           <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Latest</Link>
           <Link className='mt-8 bg-white text-green-500 font-bold p-2 md:p-3 border border-green-100 rounded shadow-xl md:shadow-xl hover:bg-green-400 hover:text-white transition-colors duration-300'>Donate</Link>
         </div>
-        <div className='lg:hidden flex '>
+        <div className='lg:hidden flex font-bold text-lg'>
           <button onClick={() => setOpen(!open)}>Menue</button>
         </div>
         <motion.div animate={open ? { opacity: 1, x: 0 } : { opacity: 0, x: "100%" }} className='absolute top-[100%] bg-black lg:hidden min-h-screen right-0 w-2/3'>
           <div className='flex flex-col gap-4 p-4'>
-            <Link className='text-[rgb(120,223,214)] '>Buy</Link>
-            <Link className='text-[rgb(120,223,214)]'>Donate</Link>
-            <Link className='text-[rgb(120,223,214)]'>Latest</Link>
+            <Link className='text-white '>Buy</Link>
+            <Link className='text-white'>Donate</Link>
+            <Link className='text-white'>Latest</Link>
           </div>
         </motion.div>
       </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,10 @@ const Home = () => {
     <div className="py-32 bg-blue-500 text-white text-center">
       <div>
         <h1 className='text-4xl font-bold'>Welcome To Our Bookstore</h1>
-        <p className="text-4xl font-bold">Explore the best collection of books from booksellers to hidden gems. find the best books to acompany your reading journey</p>
+        <p className="text-4xl ms-8 font-bold">Explore the best collection of books from booksellers to hidden gems. find the best books to acompany your reading journey</p>
+        <p className="mt-4 md:mt-8  text-xl">
+          We believe in the magic of books and their power to inspire, educate, and entertain. Dive into a world of stories and knowledge with us.
+        </p>
       </div>
     </div>
   );
