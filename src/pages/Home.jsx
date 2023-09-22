589f20b09091f6b4f6217ad367f16e21fd50829e
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-22 15:28:51 -0700

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="py-32 bg-blue-500 text-white text-center">
+    <div className="py-32 w-full flex min-h-screen justify-center items-center bg-blue-500 text-white text-center">
       <div>
         <h1 className='text-4xl font-bold'>Welcome To Our Bookstore</h1>
         <p className="text-4xl ms-8 font-bold">Explore the best collection of books from booksellers to hidden gems. find the best books to acompany your reading journey</p>
