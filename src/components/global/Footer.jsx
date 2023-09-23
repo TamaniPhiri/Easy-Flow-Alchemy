eede2ce7b116a331ebe9cc95d89d7b950da506ac
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-23 13:10:14 -0700

    change

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,12 +1,15 @@
 const Footer = () => {
   return (
-    <div className="w-full justify-center items-center flex py-20">
+    <div className="w-full justify-start px-4 flex py-20">
       <div className="rounded-xl w-1/2 flex justify-center items-center py-20 bg-gray-500">
-        <div>
-          <p className="text-xl text-white font-bold">Now Selling</p>
+        <div className="flex flex-col">
+          <p className="text-xl text-white font-bold">Buy Now</p>
           <label className="" ></label>
-          <input className="" type="text"
+          <input className="mt-8 rounded-lg py-2" type="text"
             placeholder="Enter Your Email" />
+            <select name="" id="">
+              <option value="">Payment Method</option>
+            </select>
         </div>
       </div>
     </div>
