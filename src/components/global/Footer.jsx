829f8bfbed41f133f4d5ec466f9053699c516bb2
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-22 22:51:54 -0700

    change

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full justify-center items-center flex py-20">
-      <div className="rounded-lg w-1/2 flex justify-center items-center bg-gray-700">
+      <div className="rounded-lg w-1/2 flex justify-center items-center bg-gray-500">
         <p>dfgghjjkkl</p>
       </div>
     </div>
