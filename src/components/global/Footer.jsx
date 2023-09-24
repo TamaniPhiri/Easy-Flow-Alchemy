addb955ba8ea1e55f672b8d1dbf2bcb8fba5cbd8
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-23 17:27:08 -0700

    change

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -4,19 +4,21 @@ const Footer = () => {
       <div className="rounded-xl w-full mb-0 flex justify-center items-center py-20 bg-gray-500">
         <div className="flex flex-col">
           <p className="text-xl text-white font-bold">Read Now</p>
-          <label className="" ></label>
+         
           <input className="mt-8 rounded-lg py-2" type="text"
-            placeholder="Enter Your Email" />
+            placeholder="Search Book" />
           <div className="flex flex-col">
-            <label htmlFor="">Payment
+            <label htmlFor="">Shelf
             </label>
             <select name="" id="">
-              <option value="">Airtel Money</option>
+              <option value="">New</option>
+              <option value="">Romantic</option>
+              <option value="">Poetry</option>
             </select>
             <select name="" id="">
-              <option value="">Mtn Money</option>
+              
               <select name="" id="">
-                <option value="">Zamtel Money</option>
+                
               </select>
             </select>
           </div>
