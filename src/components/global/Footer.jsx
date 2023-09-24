0c794edf4a80416cca2a958a30debd2b32948151
emmanuelnino <emmanuel98tembo@gmail.com>
2023-09-23 18:02:35 -0700

    change

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,27 +1,32 @@
 const Footer = () => {
   return (
-    <div className="w-full justify-start px-4 flex py-20">
-      <div className="rounded-xl w-full mb-0 flex justify-center items-center py-20 bg-gray-500">
+    <div className="w-full lg:justify-start justify-center gap-8 px-4 flex  py-20">
+      <div className="rounded-xl w-1/2 mb-0 flex justify-center items-center py-20 bg-blue-500">
         <div className="flex flex-col">
-          <p className="text-xl text-white font-bold">Read Now</p>
-         
-          <input className="mt-8 rounded-lg py-2" type="text"
+          <p className="text-xl text-white font-bold mb-2">Read Now</p>
+
+          <input className="mt-8 rounded-lg py-1" type="text"
             placeholder="Search Book" />
-          <div className="flex flex-col">
-            <label htmlFor="">Shelf
+          <div className="text-lg flex flex-col mt-4">
+            <label className="font-bold" htmlFor="">Shelf
             </label>
-            <select name="" id="">
-              <option value="">New</option>
+            <select className="rounded mt-4 " name="" id="">
+              <option value=""></option>
               <option value="">Romantic</option>
               <option value="">Poetry</option>
             </select>
-            <select name="" id="">
-              
-              <select name="" id="">
-                
-              </select>
-            </select>
+
+
           </div>
+        </div>
+      </div>
+      <div className="rounded-xl w-1/2 mb-0 flex justify-center items-center py-20 bg-blue-500">
+        <div className="px-6 flex flex-col">
+          <p className="text-xl text-white font-bold mb-2">Authors</p>
+
+          <input className=" mt-8 rounded-lg py-1" type="text"
+            placeholder="Search Author" />
+          
         </div>
       </div>
     </div>
